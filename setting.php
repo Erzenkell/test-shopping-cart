bff4dfffb8cc5bfec7cbc97d04f2f6f808ab60ba
Erzenkell <75608041+Erzenkell@users.noreply.github.com>
2021-05-27 16:10:47 +0200

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Esprit Shonen</title>
+	<!-- Css reset -->
+	<link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+	<!-- Global -->
+	<link rel="stylesheet" href="css/include.css">
+	<link rel="shortcut icon" href="#" />
+</head>
